71495660f8431a4fd6217c471ee0ce2067ecbd96
Aksel Danielsen <akselsd@stud.ntnu.no>
2019-05-12 00:48:27 +0200

    Added 😁.cpp

---
+++ 😁.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+
+#define 😀 std
+#define 🙌 using
+#define 😄 cout
+#define 😁 endl
+#define 😆 <<
+#define 🤣(x) 😀::😄 😆 x 😆 😀::😁
+#define 😇 main
+#define 👌 for
+#define 👄 namespace
+#define 💲 codecvt_utf8
+#define 🙊 wstring_convert
+#define 🔥 to_bytes
+#define 🔒 constexpr
+#define 💯 return
+
+👄 🚨 {
+
+🙌 💦 = wchar_t;
+🙌 😃 = int;
+🙌 😓 = 😀::💲<💦>;
+🙌 🍆 = 😀::wstring;
+
+🔒 😃 👆 = 0x1f600;
+🔒 😃 👇 = 0x1f64f;
+
+😀::🙊<😓, 💦> 🙏;
+
+}
+
+🚨::😃 😇() {
+    🙌 👄 🚨;
+    🍆 🔞;
+    👌 (😃 👈 = 👆; 👈 <= 👇; ++👈){
+        🔞 = 👈;
+        🤣(🙏.🔥(🔞));
+    }
+    💯 0;
+}
